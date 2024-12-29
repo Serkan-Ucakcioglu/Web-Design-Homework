0314d1a0941da1e3f294113ff4a01b2e81e0db33
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2024-12-29 20:59:11 +0300

    update

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-// Dropdown'ları seç
+const hamburgerBtn = document.querySelector(".icon");
 const dropdowns = document.querySelectorAll(".dropdown");
 
-// Her bir dropdown için olay dinleyicileri ekle
 dropdowns.forEach((dropdown) => {
-  const toggle = dropdown.querySelector(".dropdown-toggle");
   const menu = dropdown.querySelector(".dropdown-menu");
 
-  // Tıklama ile menüyü aç/kapat
-  toggle.addEventListener("click", (e) => {
-    e.preventDefault();
-    dropdowns.forEach((otherDropdown) => {
-      if (otherDropdown !== dropdown) {
-        otherDropdown.classList.remove("open");
-      }
-    });
-    dropdown.classList.toggle("open");
+  dropdown.addEventListener("mouseenter", () => {
+    dropdown.classList.add("open");
   });
-});
 
-// Menü dışında bir yere tıklandığında tüm menüleri kapat
-document.addEventListener("click", (e) => {
-  dropdowns.forEach((dropdown) => {
-    if (!dropdown.contains(e.target)) {
-      dropdown.classList.remove("open");
-    }
+  dropdown.addEventListener("mouseleave", (e) => {
+    if (e.relatedTarget && dropdown.contains(e.relatedTarget)) return;
+    dropdown.classList.remove("open");
   });
 });
