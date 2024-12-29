046a00217ea9573d5036b66e78baafd91fa9987b
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2024-12-29 23:24:02 +0300

    update

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const hamburgerBtn = document.querySelector(".icon");
 const dropdowns = document.querySelectorAll(".dropdown");
+const closeNavbar = document.querySelector(".close");
+const responsiveNavBar = document.querySelector(".responsive-navbar");
+const responsiveDropDown = document.querySelector(".dropdown-menu2");
+const responsiveDrop = document.querySelector(".dropdown-responsive");
+const caret = document.querySelector(".caret");
 
-dropdowns.forEach((dropdown) => {
-  const menu = dropdown.querySelector(".dropdown-menu");
+// Hamburger butonuna tıklanınca navbar'ı aç
+hamburgerBtn.addEventListener("click", () => {
+  responsiveNavBar.classList.add("open");
+});
 
-  dropdown.addEventListener("mouseenter", () => {
-    dropdown.classList.add("open");
-  });
+// Navbar'ı kapatma butonuna tıklanınca navbar'ı kapat
+closeNavbar.addEventListener("click", () => {
+  responsiveNavBar.classList.remove("open");
+});
 
-  dropdown.addEventListener("mouseleave", (e) => {
-    if (e.relatedTarget && dropdown.contains(e.relatedTarget)) return;
-    dropdown.classList.remove("open");
-  });
+// Dropdown menüsünün tıklanabilir olmasını sağla
+responsiveDrop.addEventListener("click", (e) => {
+  e.preventDefault(); // Linkin varsayılan davranışını engelle
+  responsiveDropDown.classList.toggle("toggle");
+
+  // İkonun dönüşmesini sağla
+  caret.classList.toggle("rotate");
 });
